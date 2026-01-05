0554aee0b9e52503e2caa66ee80ec633477b3c9d
joaocmfnogueira <joao.nogueira@estudante.ufjf.br>
2026-01-05 20:03:20 -0300

    terminando atividade 9.3

---
+++ exercises/Exercise09_3.js
@@ -0,0 +1,130 @@
+import * as THREE from  'three';
+import Stats from '../build/jsm/libs/stats.module.js';
+import {TrackballControls} from '../build/jsm/controls/TrackballControls.js';
+import {initRenderer, 
+        createGroundPlane,
+        createLightSphere,        
+        onWindowResize} from "../libs/util/util.js";
+
+var scene = new THREE.Scene();    // Create main scene
+var stats = new Stats();          // To show FPS information  var renderer = initRenderer();    // View function in util/utils
+var renderer = initRenderer();    // View function in util/utils
+  renderer.setClearColor("rgb(30, 30, 42)");
+
+let loader = new THREE.TextureLoader();
+
+
+var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.lookAt(0, 0, 0);
+  camera.position.set(5, 7.0, 9);
+  camera.up.set( 0, 1, 0 );
+
+var ambientLight = new THREE.AmbientLight("rgb(100, 100, 100)");
+   ambientLight.intensity = 1;
+scene.add(ambientLight);
+
+var lightPosition = new THREE.Vector3(4, 8, 0.0);
+  var light = new THREE.SpotLight(0xffffff);
+  light.position.copy(lightPosition);
+  light.castShadow = true;
+  light.penumbra = 0.5;    
+  light.intensity = 200;
+scene.add(light);
+
+var lightSphere = createLightSphere(scene, 0.1, 10, 10, lightPosition);  
+
+// Enable mouse rotation, pan, zoom etc.
+var trackballControls = new TrackballControls( camera, renderer.domElement );
+
+// Listen window size changes
+window.addEventListener( 'resize', function(){onWindowResize(camera, renderer)}, false );
+
+// Show axes (parameter is size of each axis)
+var axesHelper = new THREE.AxesHelper( 1.5 );
+  axesHelper.visible = false;
+scene.add( axesHelper );
+
+//-- Scene Objects -----------------------------------------------------------
+// Ground
+var groundPlane = createGroundPlane(5.0, 5.0, 100, 100); // width and height
+  groundPlane.rotateX(THREE.MathUtils.degToRad(-90));
+scene.add(groundPlane);
+
+// // Cube
+// var cilinderSize = 0.6;
+// var cilinderGeometry = new THREE.CylinderGeometry(cilinderSize, cilinderSize, 3 * cilinderSize);
+// var cilinderMaterial = [
+//     setMaterial('../assets/textures/wood.png', 1, 1),
+//     setMaterial('../assets/textures/woodtop.png', 1, 1)
+// ];
+// var cilinder = new THREE.Mesh(cilinderGeometry, cilinderMaterial);
+//   cilinder.castShadow = true;
+//   cilinder.position.set(0.0, cilinderSize * 1.5, 0.0);
+// scene.add(cilinder);
+
+var textureLoader = new THREE.TextureLoader();
+// Caixa
+let geometry = new THREE.BoxGeometry(2, 1.4, 2);
+let cubeMaterials = [
+    setMaterial('../assets/textures/papelao.jpg', 1, 1),
+    setMaterial('../assets/textures/papelao.jpg', 1, 1),
+    null,
+    setMaterial('../assets/textures/papelao.jpg', 1, 1),
+    setMaterialWithOpacity(),
+    setMaterial('../assets/textures/papelao.jpg', 1, 1)
+];
+let cube = new THREE.Mesh(geometry, cubeMaterials);
+cube.position.y = 0.75;
+cube.castShadow = true;
+cube.receiveShadow = true;
+scene.add(cube);
+
+// Bola
+let ballGeometry = new THREE.SphereGeometry(0.8);
+let ballMaterial = setMaterial('../assets/textures/basket.png', 1, 1);
+let ball = new THREE.Mesh(ballGeometry, ballMaterial);
+ball.position.y = 0.8;
+ball.castShadow = true;
+ball.receiveShadow = true;
+scene.add(ball);
+
+//----------------------------------------------------------------------------
+//-- Use TextureLoader to load texture files
+
+var floor  = textureLoader.load('../assets/textures/floorWood.jpg');
+    floor.colorSpace = THREE.SRGBColorSpace;
+
+
+var sun = textureLoader.load('../assets/textures/sun.jpg');
+
+function setMaterial(file, repeatU = 1, repeatV = 1, color = 'rgb(255,255,255)'){
+   let mat = new THREE.MeshLambertMaterial({ map: loader.load(file), color:color});
+      mat.map.colorSpace = THREE.SRGBColorSpace;
+   mat.map.wrapS = mat.map.wrapT = THREE.RepeatWrapping;
+   mat.map.minFilter = mat.map.magFilter = THREE.LinearFilter;
+   mat.map.repeat.set(repeatU,repeatV); 
+   mat.side = 2;
+   return mat;
+}
+
+function setMaterialWithOpacity(){
+    let mat = new THREE.MeshLambertMaterial({ color:'rgba(10, 126, 161, 1)'});
+    mat.side = 2;
+    mat.transparent = true;
+    mat.opacity = 0.5;
+    return mat;
+}
+// Apply texture to the 'map' property of the respective materials' objects
+groundPlane.material.map = floor;
+lightSphere.material.map = sun;
+
+render();
+
+
+function render()
+{
+  stats.update();
+  trackballControls.update();
+  requestAnimationFrame(render);
+  renderer.render(scene, camera)
+}
